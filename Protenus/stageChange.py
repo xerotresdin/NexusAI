cc86665df03ef470dae89dc9af10c9c213b45658
Laila Paredes <lparede2@ugradx.cs.jhu.edu>
2023-12-02 12:37:56 -0500

    closed won + id

--- Protenus/stageChange.py
+++ Protenus/stageChange.py
@@ -1,49 +1,4 @@
 import csv
-<<<<<<< HEAD
-from collections import defaultdict
-import pandas as pd
-
-# Read dates from file
-with open('2023_dates.txt', 'r') as date_file:
-    dates = [line.strip() for line in date_file if line.strip()]
-
-# Read stages from file
-with open('stages.txt', 'r') as stages_file:
-    stages = [line.strip() for line in stages_file if line.strip()]
-
-# Combine dates and stages into a defaultdict
-data = defaultdict(list)
-closed_won_count = 0  # Track the total number of "closed won" cases
-
-for date, stage in zip(dates, stages):
-    quarter = pd.to_datetime(date).to_period("Q")
-    data[quarter].append({'date': date, 'stage': stage})
-
-    # Check if the case is "closed won"
-    if stage == 'Closed Won':
-        closed_won_count += 1
-
-# Calculate the total number of entries
-total_entries = len(dates)
-
-# Calculate the percentage of total "closed won" cases
-closed_won_percentage_total = (closed_won_count / total_entries) * 100
-
-# Calculate the percentage of "closed won" cases for every quarter
-quarterly_closed_won_percentages = {}
-
-for quarter, entries in data.items():
-    closed_won_count_quarter = sum(entry['stage'] == 'closed won' for entry in entries)
-    total_entries_quarter = len(entries)
-    closed_won_percentage_quarter = (closed_won_count_quarter / total_entries_quarter) * 100
-    quarterly_closed_won_percentages[quarter] = closed_won_percentage_quarter
-
-# Output results
-print(f'Total entries: {total_entries}')
-print(f'Percentage of total "closed won" cases: {closed_won_percentage_total:.2f}%')
-
-=======
->>>>>>> refs/remotes/origin/main
 import datetime
 from collections import defaultdict
 
@@ -76,13 +31,6 @@ for i, date_str in enumerate(dates):
     except ValueError:
         print(f"Invalid date format: {date_str}")
 
-<<<<<<< HEAD
-# Output the dates in each quarter by year
-for year, quarters in quarters_by_year.items():
-    print(f"Year: {year}")
-    for quarter, dates in quarters.items():
-        print(f"  {quarter}: {dates}")
-=======
 # Read other data from files
 with open('ID.txt', 'r') as hash_file:
     hash_codes = [line.strip() for line in hash_file if line.strip()]
@@ -100,20 +48,27 @@ for i, (hash_code, state, zip_code, stage) in enumerate(zip(hash_codes, states, 
     if i in date_quarter_year_mapping:
         quarter, year = date_quarter_year_mapping[i]
         key = (state, zip_code)
-        data_by_quarter_and_year[(quarter, year)][key].append({'hash_code': hash_code, 'stage': stage})
+        data_by_quarter_and_year[(quarter, year)][key].append({'hash_code': hash_code, 'stage': stage, 'date': dates[i]})
 
-# Calculate and print statistics for each quarter and year
-for (quarter, year), data in data_by_quarter_and_year.items():
-    total_entries = sum(len(v) for v in data.values())
-    unique_entries = len(data)
-    duplicate_percentage = ((total_entries - unique_entries) / total_entries) * 100
-    # Assuming "Stage 5 - Contracting" as "Closed Won"
-    closed_won_count = sum(len([entry for entry in v if entry['stage'] == 'Closed Won']) for v in data.values())
-    closed_won_percentage_total = (closed_won_count / total_entries) * 100
+# Save closed-won cases to CSV file
+csv_file_path = 'closed_won_cases.csv'
+with open(csv_file_path, 'w', newline='') as csvfile:
+    fieldnames = ['ID', 'Quarter', 'Date', 'Stage']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
-    print(f"Year: {year}, Quarter: {quarter}")
-    print(f"  Total entries: {total_entries}")
-    print(f"  Unique entries: {unique_entries}")
-    print(f"  Percentage of duplicate entries: {duplicate_percentage:.2f}%")
-    print(f"  Percentage of total 'closed won' cases: {closed_won_percentage_total:.2f}%")
->>>>>>> refs/remotes/origin/main
+    # Write header
+    writer.writeheader()
+
+    # Write closed-won cases
+    for (quarter, year), data in data_by_quarter_and_year.items():
+        for entries in data.values():
+            for entry in entries:
+                if entry['stage'] == 'Closed Won':
+                    writer.writerow({
+                        'ID': entry['hash_code'],
+                        'Quarter': f'{quarter} {year}',
+                        'Date': entry['date'],
+                        'Stage': entry['stage']
+                    })
+
+print(f'Closed-won cases saved to {csv_file_path}')
