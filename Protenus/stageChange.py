c8180ee807b9088ded60101e00a2a9e3ecc665ba
Laila Paredes <lparede2@ugradx.cs.jhu.edu>
2023-12-02 11:59:16 -0500

    git merger

--- Protenus/stageChange.py
+++ Protenus/stageChange.py
@@ -1,43 +1,46 @@
 import csv
 from collections import defaultdict
+import pandas as pd
 
-# Read hash codes from file
-with open('ID.txt', 'r') as hash_file:
-    hash_codes = [line.strip() for line in hash_file if line.strip()]
-
-# Read zip codes from file
-with open('zip.txt', 'r') as zip_file:
-    zip_codes = [line.strip() for line in zip_file if line.strip()]
-
-# Read states from file
-with open('state.txt', 'r') as state_file:
-    states = [line.strip() for line in state_file if line.strip()]
+# Read dates from file
+with open('2023_dates.txt', 'r') as date_file:
+    dates = [line.strip() for line in date_file if line.strip()]
 
 # Read stages from file
 with open('stages.txt', 'r') as stages_file:
     stages = [line.strip() for line in stages_file if line.strip()]
 
-# Combine hash codes, states, zip codes, and stages into a defaultdict
+# Combine dates and stages into a defaultdict
 data = defaultdict(list)
 closed_won_count = 0  # Track the total number of "closed won" cases
 
-for hash_code, state, zip_code, stage in zip(hash_codes, states, zip_codes, stages):
-    key = (state, zip_code)
-    data[key].append({'hash_code': hash_code, 'stage': stage})
-    
+for date, stage in zip(dates, stages):
+    quarter = pd.to_datetime(date).to_period("Q")
+    data[quarter].append({'date': date, 'stage': stage})
+
     # Check if the case is "closed won"
-    if stage == 'Closed Won':
+    if stage == 'closed won':
         closed_won_count += 1
 
-# Calculate the percentage of duplicate zip codes and states
-total_entries = len(hash_codes)
-unique_entries = len(data)
-duplicate_percentage = ((total_entries - unique_entries) / total_entries) * 100
+# Calculate the total number of entries
+total_entries = len(dates)
 
 # Calculate the percentage of total "closed won" cases
 closed_won_percentage_total = (closed_won_count / total_entries) * 100
 
+# Calculate the percentage of "closed won" cases for every quarter
+quarterly_closed_won_percentages = {}
+
+for quarter, entries in data.items():
+    closed_won_count_quarter = sum(entry['stage'] == 'closed won' for entry in entries)
+    total_entries_quarter = len(entries)
+    closed_won_percentage_quarter = (closed_won_count_quarter / total_entries_quarter) * 100
+    quarterly_closed_won_percentages[quarter] = closed_won_percentage_quarter
+
+# Output results
 print(f'Total entries: {total_entries}')
-print(f'Unique entries: {unique_entries}')
-print(f'Percentage of duplicate entries: {duplicate_percentage:.2f}%')
 print(f'Percentage of total "closed won" cases: {closed_won_percentage_total:.2f}%')
+
+print("\nClosed Won Percentages for Each Quarter:")
+for quarter, percentage in quarterly_closed_won_percentages.items():
+    print(f'{quarter}: {percentage:.2f}%')
