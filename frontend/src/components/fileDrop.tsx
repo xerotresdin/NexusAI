565bae6e411d3c1806f667645c5bfdd37d12b67d
xerotresdin <wli111@jh.edu>
2023-12-02 14:06:43 -0500

    Frontend added

--- frontend/src/components/fileDrop.tsx
+++ frontend/src/components/fileDrop.tsx
@@ -8,7 +8,6 @@ const FileDrop: React.FC = () => {
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-    // Check if the file being dragged is a .csv file
     const files = e.dataTransfer.items;
     if (files.length > 0 && files[0].kind === 'file' && files[0].type === 'text/csv') {
       setIsCsvFileHovered(true);
@@ -22,7 +21,7 @@ const FileDrop: React.FC = () => {
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-    setIsCsvFileHovered(false); // Reset on drop
+    setIsCsvFileHovered(false);
     let files = e.dataTransfer.files;
     if (files.length > 0 && files[0].type === 'text/csv') {
       setFile(files[0]);
@@ -33,42 +32,36 @@ const FileDrop: React.FC = () => {
   };
 
   return (
-    <div id="app" className="container">
-      <div className="fixed left-[50%] top-[50%] max-h-full w-full translate-x-[-50%] translate-y-[-50%] overflow-y-auto p-5 text-center sm:max-w-2xl">
-        <div className="mb-6 text-4xl font-bold font-unbounded md:text-5xl">
-          Use the NexusAI!
+    <div id="app" className="container" style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh' }}>
+      <header style={{ marginBottom: '20px' }}>
+        <div style={{ fontSize: '32px', fontWeight: 'bold', marginBottom: '24px' }}>
+          Try NexusAI!
         </div>
-        <p className="mb-10 text-sm text-black-300 ">
-        This project targets the productivity challenges faced by professionals overwhelmed with meetings, emails, 
-        and messages. Our application, currently in beta, automates recording of voice notes and text notes, enabling users to
-        effortlessly retrieve relevant information from these notes, regardless of their creation time. This tool provides a 
-        swift and simple solution for users to effectively handle their information overload.
+        <p style={{ marginBottom: '40px', fontSize: 'small', color: '#4a4a4a' }}>
+          NexusAI is ...
         </p>
-        
+      </header>
 
-        
-      </div>
       <div className="drag-drop-box" 
            onDragOver={handleDragOver} 
            onDragLeave={handleDragLeave}
            onDrop={handleDrop} 
            style={{ 
-             margin: 'auto', 
+             margin: '20px auto', 
              padding: '50px', 
-             textAlign: 'center', 
              border: '2px dashed #ccc', 
              borderRadius: '20px', 
              color: 'gray', 
-             backgroundColor: isCsvFileHovered ? '#D3D3D3' : '#F4F4F4', // Change color when .csv is hovered
-             boxShadow: isCsvFileHovered ? '0 0 10px #333' : 'none', // Optional shadow effect
+             backgroundColor: isCsvFileHovered ? '#D3D3D3' : '#F4F4F4',
+             boxShadow: isCsvFileHovered ? '0 0 10px #333' : 'none',
+             maxWidth: '600px'
            }}>
         {message}
-        
       </div>
-      <footer className="mt-5 text-xs">
-          &copy; 2023 NexusAI inc. All rights reserved
-        </footer>
-      
+
+      <footer style={{ marginTop: 'auto', fontSize: 'x-small' }}>
+        &copy; 2023 NexusAI Inc. All rights reserved
+      </footer>
     </div>
   );
 };
